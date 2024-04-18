d978eccc789016fa354fce836c9cb74f8cbf22aa
Xavier Mitault <xavier.mitault@smobe.fr>
2024-04-18 11:22:12 +0200

    Change benchmark

--- benchmark/benchmark.md
+++ benchmark/benchmark.md
@@ -13,10 +13,10 @@
 - vaytracer-dev
   The raytracer in this directory. Builded with `make vaytracer-dev`
 
-### [../scenes/basic1.toml](../scenes/basic1.toml)
+### [./basic1.toml](./basic1.toml)
 
 | Command | Mean [ms] | Min [ms] | Max [ms] | Relative |
 |:---|---:|---:|---:|---:|
-| `./vaytracer-gcc --scene-file '../scenes/basic1.toml' --output-file 'output_vaytracer-gcc_basic1.toml.ppm'` | 61.8 ± 5.1 | 56.8 | 91.4 | 1.00 |
-| `./vaytracer-clang --scene-file '../scenes/basic1.toml' --output-file 'output_vaytracer-clang_basic1.toml.ppm'` | 78.1 ± 3.5 | 73.9 | 93.2 | 1.26 ± 0.12 |
-| `./vaytracer-dev --scene-file '../scenes/basic1.toml' --output-file 'output_vaytracer-dev_basic1.toml.ppm'` | 702.1 ± 8.2 | 692.6 | 723.1 | 11.37 ± 0.94 |
+| `./vaytracer-gcc --scene-file './basic1.toml'` | 167.1 ± 5.2 | 155.6 | 184.7 | 1.00 |
+| `./vaytracer-clang --scene-file './basic1.toml'` | 179.0 ± 20.7 | 161.5 | 305.4 | 1.07 ± 0.13 |
+| `./vaytracer-dev --scene-file './basic1.toml'` | 1642.0 ± 71.5 | 1600.4 | 2048.5 | 9.83 ± 0.53 |
--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -5,7 +5,7 @@
 programs="vaytracer-clang,vaytracer-gcc,vaytracer-dev"
 
 scenes=(
-    "../scenes/basic1.toml"
+    "./basic1.toml"
 )
 
 rm -f "benchmark.md"
@@ -32,36 +32,23 @@ EOF
 for scene in "${scenes[@]}"; do
     sleep 10
     benchmark_file="benchmark-$(basename ${scene}).md"
-    rm -f "${benchmark_file}"
-    output_file="output_{program}_$(basename ${scene}).ppm"
+    rm -f -- "${benchmark_file}"
+    output_file="$(grep "output = " ${scene} | cut -f2 -d'"').ppm"
     command_bench='./{program}'
     command_bench="${command_bench} --scene-file '${scene}'"
-    command_bench="${command_bench} --output-file '${output_file}'"
     hyperfine \
         --shell none \
         --min-runs 100 \
         --warmup 50 \
         --sort mean-time \
-        --cleanup 'sleep 10' \
+        --cleanup "rm -- ${output_file}; sleep 10" \
         --export-markdown "${benchmark_file}" \
         --parameter-list program "${programs}" \
-        --prepare "rm -f ${output_file}" \
+        --prepare "rm -f -- ${output_file}" \
         "${command_bench}"
     errors="0"
-    for file in output_*_*.ppm; do
-        if ! magick convert "${file}" test.png; then
-            echo "Bad format: ${file}"
-            errors="1"
-        else
-            echo "Ok format: ${file}"
-            rm "${file}"
-            rm "test.png"
-        fi
-    done
-    if [[ "${errors}" == "0" ]]; then
-        echo "### [${scene}](${scene})" >> "benchmark.md"
-        echo "" >> "benchmark.md"
-        cat "${benchmark_file}" >> "benchmark.md"
-        rm "${benchmark_file}"
-    fi
+    echo "### [${scene}](${scene})" >> "benchmark.md"
+    echo "" >> "benchmark.md"
+    cat "${benchmark_file}" >> "benchmark.md"
+    rm -- "${benchmark_file}"
 done
