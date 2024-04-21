fb66887a767bfd93e7b8771484a77ad701e07d30
Xavier Mitault <xavier.mitault@epitech.eu>
2024-04-21 13:53:16 +0200

    Add scene in shared context

--- benchmark/benchmark.md
+++ benchmark/benchmark.md
@@ -17,3 +17,7 @@
 
 ### [./basic1.toml](./basic1.toml)
 
+| Command | Mean [ms] | Min [ms] | Max [ms] | Relative |
+|:---|---:|---:|---:|---:|
+| `./vaytracer-clang --quiet --scene-file './basic1.toml'` | 363.8 ± 58.9 | 226.2 | 411.9 | 1.00 |
+| `./vaytracer-gcc --quiet --scene-file './basic1.toml'` | 382.0 ± 4.1 | 371.5 | 392.3 | 1.05 ± 0.17 |
--- src/render_pool.v
+++ src/render_pool.v
@@ -10,12 +10,12 @@ pub struct Params {
 	x int
 	y int
 	cam_i int
-	scene vtc.Scene
 }
 
 fn sprocess(mut pp pool.PoolProcessor, idx int, wid int) &gg.Color {
     item := pp.get_item[Params](idx)
-	color := item.scene.calculate_pixel(item.cam_i, item.x, item.y)
+	scene := unsafe { &vtc.Scene(pp.get_shared_context()) }
+	color := scene.calculate_pixel(item.cam_i, item.x, item.y)
     return &gg.Color{
 		r: color.r
 		g: color.g
@@ -26,6 +26,7 @@ fn sprocess(mut pp pool.PoolProcessor, idx int, wid int) &gg.Color {
 
 fn render_pool_camera(cam_i int, scene vtc.Scene) ! {
 	mut pp := pool.new_pool_processor(callback: sprocess)
+	pp.set_shared_context(scene)
 	header := 'P3 ${scene.cameras[cam_i].width} ${scene.cameras[cam_i].height} 255\n'
 	mut output_string := strings.new_builder(header.len + (scene.cameras[cam_i].height * scene.cameras[cam_i].width) * 4 * 3)
 	output_string.write_string(header)
@@ -36,7 +37,6 @@ fn render_pool_camera(cam_i int, scene vtc.Scene) ! {
 				x: x
 				y: y
 				cam_i: cam_i
-				scene: scene
 			}
 		}
 	}
